64773c4f466afe7b25cc440e233cb82c6a41e4c7
ysmr4kir <akatukinosora11@gmail.com>
2026-06-04 20:56:19 +0900

    Update auth.php

--- php/auth.php
+++ php/auth.php
@@ -1,8 +1,22 @@
 <?php
 /*====================
+セッション開始関数
+====================*/
+function start_sess(){
+    if(session_status() == PHP_SESSION_NONE){
+        ini_set('session.cookie_httponly', 1); // JavaScriptからクッキーへのアクセスを禁止
+        ini_set('session.use_strict_mode', 1); // セッションIDの固定攻撃を防止
+        ini_set('session.cookie_secure', 1); // HTTPS接続でのみクッキーを送信
+        ini_set('session.cookie_samesite', 'Strict'); // クロスサイトリクエストを防止
+
+        session_start();
+    }
+}
+
+/*====================
 セッション破棄関数
 ====================*/
-function session_del(){
+function del_sess(){
     //セッションが開始されていない場合は開始する
     if(session_status() == PHP_SESSION_NONE){
         session_start();
@@ -30,7 +44,7 @@ function session_del(){
 function login_check(){
     //セッションが開始されていない場合は開始する
     if(session_status() == PHP_SESSION_NONE){
-        session_start();
+        start_sess();
     }
 
     //セッションにユーザーIDが保存されているか確認する
@@ -46,10 +60,49 @@ function login_check(){
         $timeout = 30 * 60; // 30分
         if(time() - $_SESSION['last_acc'] > $timeout){
             //セッションを破棄してログインページにリダイレクトする
-            session_del();
+            del_sess();
             $_SESSION['return_to'] = $_SERVER['REQUEST_URI'];
             header('Location: login.php');
             exit();
         }
     }
+}
+
+/*====================
+CSRFトークン生成関数
+====================*/
+function generate_csrf(){
+    //セッションが開始されていない場合は開始する
+    if(session_status() == PHP_SESSION_NONE){
+        start_sess();
+    }
+
+    //CSRFトークンを生成してセッションに保存する
+    if(empty($_SESSION['csrf_token'])){
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+    return $_SESSION['csrf_token'];
+}
+
+/*====================
+CSRFトークンチェック関数
+====================*/
+/* 呼び出すときは
+check_csrf($_POST['csrf_token'])のように、フォームから送られてきたトークンを引数に渡す
+*/
+function check_csrf($token){
+    //セッションが開始されていない場合は開始する
+    if(session_status() == PHP_SESSION_NONE){
+        start_sess();
+    }
+
+    //セッションに保存されたCSRFトークンと一致するか確認する
+    if(isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token)){
+        return true;
+    }else{
+        unset($_SESSION['csrf_token']);// トークンが不正な場合はセッションから削除して新しいトークンを生成する
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32)); // 新しいトークンを生成して保存する
+        die('不正なリクエストです。');
+    }
+
 }
